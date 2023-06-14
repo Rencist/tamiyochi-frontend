0cb157ebd3420498de47576eab2890b05b2d2379
Aryan Shafa Wardana <aryan.s.wardana@gmail.com>
2023-06-14 19:45:16 +0700

    feat: detail manga integration

--- src/pages/manga/[mangaid].page.tsx
+++ src/pages/manga/[mangaid].page.tsx
@@ -1,7 +1,7 @@
 import { useQuery } from '@tanstack/react-query';
 import Image from 'next/image';
 import { useRouter } from 'next/router';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 
 import Typography from '@/components/typography/Typography';
 import Layout from '@/layouts/Layout';
@@ -13,66 +13,40 @@ import { ApiReturn } from '@/types/api';
 import { Seri } from '@/types/entity/manga';
 
 export default function DetailManga() {
-  const [isImageValid, setImageValid] = useState<boolean>(false);
+  const [src, setSrc] = useState('');
 
   const { mangaid } = useRouter().query;
+
   const url = `/seri/${mangaid}`;
-  const { data: mangaData } = useQuery<ApiReturn<Seri>>([url]);
+  const { data: mangaData, refetch } = useQuery<ApiReturn<Seri>>([url], {
+    onSuccess: (res) => setSrc(res.data.foto),
+  });
 
-  useEffect(() => {
-    const fetchImage = async () =>
-      mangaData?.data.foto &&
-      (await fetch(mangaData?.data.foto)
-        .then((res) => {
-          if (res.ok) {
-            setImageValid(true);
-          }
-        })
-        .catch(() => {
-          return;
-        }));
-
-    fetchImage();
-  }, [mangaData?.data.foto]);
   return (
     <Layout withNavbar={true}>
       <main className='min-h-screen'>
         <div className='relative h-[300px] w-full -z-50'>
-          {isImageValid && mangaData?.data.foto ? (
-            <Image
-              src={mangaData?.data.foto}
-              alt='Manga Foto'
-              className='w-full h-full object-cover blur-sm'
-              fill
-            />
-          ) : (
-            <Image
-              src='/images/error.jpg'
-              alt='manga-cover'
-              className='w-full h-full object-cover object-center'
-              fill
-            />
-          )}
+          <Image
+            src={src}
+            alt='manga-cover'
+            width='200'
+            height='300'
+            onError={() => setSrc('/images/error.jpg')}
+            className='w-full h-full object-cover blur-sm'
+          />
           <div className='absolute top-0 bg-teal-900 opacity-50 w-full h-full' />
         </div>
 
         <section className='flex flex-row gap-8 px-12 py-8 bg-base-surface border'>
           <div className='relative w-[208px] h-[300px] -mt-24'>
-            {isImageValid && mangaData?.data.foto ? (
-              <Image
-                src={mangaData?.data.foto}
-                alt='Manga Foto'
-                className='rounded-xl'
-                fill
-              />
-            ) : (
-              <Image
-                src='/images/error.jpg'
-                alt='manga-cover'
-                className='object-cover'
-                fill
-              />
-            )}
+            <Image
+              src={src}
+              alt='manga-cover'
+              width='200'
+              height='300'
+              onError={() => setSrc('/images/error.jpg')}
+              className='w-full h-full object-cover rounded-xl'
+            />
           </div>
           <div className='flex-1 flex flex-col gap-3'>
             <Typography
@@ -103,11 +77,11 @@ export default function DetailManga() {
             />
 
             <div className='flex flex-col gap-8 w-full'>
-              <Volume manga={mangaData.data.manga} />
+              <Volume manga={mangaData.data.manga} refetch={refetch} />
 
-              <Score />
+              <Score seriId={mangaData.data.id} />
 
-              {mangaid && <Comment mangaId={mangaid[0]} />}
+              {mangaid && <Comment mangaId={+mangaid} />}
             </div>
           </section>
         )}
